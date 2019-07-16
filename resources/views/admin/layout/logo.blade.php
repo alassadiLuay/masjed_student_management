e75d9fb63a9481eca650faa48627ac5311a6f144
hsoub <hsoub.academy@gmail.com>
2019-07-16 23:49:11 +0300

    Change Title

--- resources/views/admin/layout/logo.blade.php
+++ resources/views/admin/layout/logo.blade.php
@@ -1,6 +1,6 @@
 <a href="{{ url('admin') }}" class="navbar-brand">
     {{-- You may use plain text as a logo instead of image --}}
-    <img src="{{url('/images/out-source/icon.jpg')}}" alt="" style="width: 45px;max-height: 35px;">
+    <img src="{{url('/images/out-source/icon.png')}}" alt="" style="width: 45px;max-height: 35px;">
 
-مسجد الشمسية
+مسجد بدر
 </a>
